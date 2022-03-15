23b4a21273231621e1923af68ce656c21c9e9689
maxexee <ruizmmaxm@gmail.com>
2022-03-15 10:02:14 -0600

    Tema 4, practica 1

---
+++ plf_Programas/t4/RuizManjarrez_Maximiliano_practica1.py
@@ -0,0 +1,25 @@
+from cProfile import label
+import  numpy as np
+import  matplotlib.pylab as plt
+
+'''def h(x):
+    return  np.sin(x)
+
+x   =   np.linspace(0,  10, 100)
+
+plt.grid()
+plt.xlabel('Tiempo')
+plt.ylabel('Posición')
+plt.title('Función de seno')
+plt.plot(x, h(x),    'r--', label='seno')
+#plt.show()'''
+
+x   =   np.linspace(0,  10, 100)
+
+g   =   lambda x:   np.tan(x)
+
+plt.plot(x, g(x),   label='Coseno de x')
+v   =   [0, 10, -2, 2]
+plt.axis(v)
+plt.legend(loc  =   3)
+plt.show()
